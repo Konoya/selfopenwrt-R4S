019961e31d59450eb4a6c10bf95a7ae139738649
Konoya <69484973+Konoya@users.noreply.github.com>
2021-01-06 21:48:32 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,4 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git openwrtpasswall https://github.com/xiaorouji/openwrt-passwall.git
-src-git sirpdboypackage https://github.com/siropboy/sirpdboy-package.git' feeds.conf.default
+sed -i '$a src-git openwrtpasswall https://github.com/xiaorouji/openwrt-passwall.git src-git sirpdboypackage https://github.com/siropboy/sirpdboy-package.git' feeds.conf.default
